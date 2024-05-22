0f46c552639be259c72f28ded58a3111b32ad84c
tushar27 <tushar.27041994@gmail.com>
2024-05-23 00:03:14 +0530

    req

---
+++ main.py
@@ -0,0 +1,4 @@
+##© 2024 Tushar Aggarwal. All rights reserved.(https://tushar-aggarwal.com)
+##My Precious with Gemini[Towards-GenAI] (https://github.com/Towards-GenAI)
+##################################################################################################
+#Importing dependencies
